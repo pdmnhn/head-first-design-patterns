6efe8035226df5cc14c95215e2304d58cd15a789
Padmanabhan K <dev.pdmnhn@gmail.com>
2024-08-27 22:41:59 +0530

    Add observer pattern

---
+++ src/main/java/dev/pdmnhn/patterns/observer/displays/StatisticsTemperatureDisplay.java
@@ -0,0 +1,29 @@
+package dev.pdmnhn.patterns.observer.displays;
+
+import dev.pdmnhn.patterns.observer.Observer;
+import dev.pdmnhn.patterns.observer.weather.WeatherData;
+
+
+public class StatisticsTemperatureDisplay implements Observer, DisplayElement {
+    private float minTemperature = Float.MAX_VALUE, maxTemperature = Float.MIN_VALUE;
+    private final WeatherData weatherData;
+
+    public StatisticsTemperatureDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update() {
+        float currentTemperature = weatherData.getTemperature();
+        minTemperature = Math.min(currentTemperature, minTemperature);
+        maxTemperature = Math.max(currentTemperature, maxTemperature);
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Min Temperature: " + minTemperature + "F degrees, Max Temperature: " + maxTemperature +
+                "F degrees");
+    }
+}
---
+++ src/main/java/dev/pdmnhn/patterns/observer/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Design Principles,
+ * <p>
+ * 1. Strive for loosely coupled designs between objects that interact.
+ */
+package dev.pdmnhn.patterns.observer;
